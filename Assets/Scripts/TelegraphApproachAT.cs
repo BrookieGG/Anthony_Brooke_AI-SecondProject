2aa6aac15b652bbc5443be780eb987a2401ab6c7
BrookieGG <brooke4444@me.com>
2026-03-22 22:54:09 -0400

    Drone turns slowly to indicate the direction of where its traveling to next

---
+++ Assets/Scripts/TelegraphApproachAT.cs
@@ -0,0 +1,77 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class TelegraphApproachAT : ActionTask {
+
+        public BBParameter<float> speed;
+        public BBParameter<Transform> targetTransform;
+
+        public BBParameter<float> rotationSpeed;
+        public float moveAngle = 20f;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+			//EndAction(true);
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+            if (targetTransform.value == null)
+            {
+                EndAction(false);
+                return;
+            }
+            
+            //Move the object towards the target Transform
+
+            Vector3 directionToMove = targetTransform.value.position - agent.transform.position;
+
+            if (directionToMove.sqrMagnitude < 0.0001f)
+            {
+                EndAction(true);
+                return;
+            }
+
+            directionToMove.y = 0f;
+
+            Quaternion targetRotation = Quaternion.LookRotation(directionToMove);
+            agent.transform.rotation = Quaternion.RotateTowards(agent.transform.rotation, targetRotation, rotationSpeed.value * Time.deltaTime);
+
+            float angle = Vector3.Angle(agent.transform.forward, directionToMove);
+
+            if (angle < moveAngle)
+            {
+                agent.transform.position += directionToMove.normalized * speed.value * Time.deltaTime;
+            }
+
+            float distanceToTarget = directionToMove.magnitude;
+
+            if (distanceToTarget < 0.5f)
+            {
+                EndAction(true);
+            }
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
